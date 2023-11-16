1575af504b129c152da21727f472cc91021be333
SeungMinJeon <100692591+HermitCrapOfLine@users.noreply.github.com>
2023-11-16 20:13:52 +0900

    11월 16일 --jsp 관련 파일-- header1 변경 1 footer1 변경 1 home.jsp 변경 1 login.jsp 변경 1
    -- image 파일--
    AlertravelLogo 추가 1
    AlertravelMainLogo 추가 1
    kakao_login_medium_wide 추가 1
    
    -- java 파일 --
    domain : AlarmLvlCriteria, AlarmVO 추가 1
    mapper : AlarmMapper 추가 1
    service : AlarmService, AlarmServiceImpl 추가 1
    controller : AlarmController 추가 1
    
    -- xml 파일 --
    AlarmMapper 추가

---
+++ alertravel/src/main/java/org/dawin/mapper/AlarmMapper.java
@@ -0,0 +1,16 @@
+package org.dawin.mapper;
+
+import java.util.List;
+
+import org.dawin.domain.AlarmLvlCriteria;
+import org.dawin.domain.AlarmVO;
+
+public interface AlarmMapper {
+
+	public List<AlarmVO> getList();
+
+	public List<AlarmVO> getListLvl(Long alarmLvl);
+	
+	public AlarmVO read(String countryEngNm);
+	
+}
